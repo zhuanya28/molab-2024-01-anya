77dd58866ef7e7efcca00800a2bbab663c729c0b
John Henry Thompson <jht9629@nyu.edu>
2024-02-26 09:00:41 -0500

    Update ContentView.swift

--- week03/homeworkW3/homeworkW3/ContentView.swift
+++ week03/homeworkW3/homeworkW3/ContentView.swift
@@ -8,29 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var randomColors: [Color] = []
+    //    @State private var randomColors: [Color] = []
     @State private var imageNames = ["star.fill", "circle.fill", "square.fill", "triangle.fill", "beats.studiobuds.chargingcase", "music.mic.circle.fill", "octagon.lefthalf.filled", "arrow.up.to.line.circle.fill"]
-//    @State private var  colors : [UIColor] = [.green, .orange, .red, .black, .blue, .brown, .white, .purple]
+        @State private var  colors : [UIColor] = [.green, .orange, .red, .black, .blue, .brown, .white, .purple]
     
     func generateRandomData() {
         imageNames.shuffle()
-//        colors.shuffle()
+        //        colors.shuffle()
     }
-
+    
     var body: some View {
         VStack {
+//            ForEach(colors, id: \.self) { item in
+//                
+//                
+//            }
             ForEach(imageNames.indices, id: \.self) { index in
-                           if let imageNameIndex = imageNames.firstIndex(of: imageNames[index]) {
-//                               let currentColor = colors[imageNameIndex]
-                               Image(systemName: imageNames[index])
-                                   .resizable()
-                                   .aspectRatio(contentMode: .fit)
-                                   .frame(width: 50, height: 50)
-                                   .foregroundColor(Color.random())
-                                   .padding()
-                           }
-                       }
-
+                if let imageNameIndex = imageNames.firstIndex(of: imageNames[index]) {
+                    //                               let currentColor = colors[imageNameIndex]
+                    Image(systemName: imageNames[index])
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(Color.random())
+                        .padding()
+                }
+            }
+            
             Button("Generate new pattern!") {
                 self.generateRandomData()
             }

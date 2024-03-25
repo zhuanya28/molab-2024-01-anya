7af844b6eae11825717f287a11fce612ec21c0b1
anya <azhukova1402@gmail.com>
2024-03-24 22:58:43 -0400

    added: week 7

---
+++ week07/week07/week07/ContentView.swift
@@ -0,0 +1,53 @@
+
+
+import UIKit
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    @State var selectedImage: UIImage?
+    @State var backgroundColor: Color = .white
+    @State var imagePicked = false
+    
+    var body: some View {
+        ZStack {
+            if let image = selectedImage {
+                ZStack {
+                    Color.clear
+                    ImageView(uiImage: image)
+                }
+                .background(backgroundColor) // Apply the background color here
+                .edgesIgnoringSafeArea(.all)
+                
+                Button("Change Photo") {
+                    imagePicked = true
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding()
+                .offset(y: -300)
+            } else {
+                Button("Select Photo") {
+                    imagePicked = true
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding()
+            }
+        }
+        .sheet(isPresented: $imagePicked) {
+            PhotoPicker(selectedImage: $selectedImage, backgroundColor: $backgroundColor)
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week07/week07/week07/week07App.swift
@@ -0,0 +1,17 @@
+//
+//  week07App.swift
+//  week07
+//
+//  Created by anya zhukova on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct week07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

141f1e612bdbeb87b107b09a1985e4e5aa83cafe
anya <azhukova1402@gmail.com>
2024-04-08 12:09:40 -0400

    fixed: preview & reminders page

--- week09/week09/RemindersPage.swift
+++ week09/week09/RemindersPage.swift
@@ -8,48 +8,56 @@
 import SwiftUI
 
 struct RemindersPage: View {
-  @State
-  private var reminders = Reminder.samples
-
-  @State
-  private var isAddReminderDialogPresented = false
-
-  private func presentAddReminderView() {
-    isAddReminderDialogPresented.toggle()
-  }
-
-  var body: some View {
-    List($reminders) { $reminder in
-      HStack {
-        Image(systemName: reminder.isCompleted
-              ? "largecircle.fill.circle"
-              : "circle")
-          .imageScale(.large)
-          .foregroundColor(.accentColor)
-          .onTapGesture {
-            reminder.isCompleted.toggle()
-          }
-        Text(reminder.title)
-      }
+    @State
+    private var reminders = Reminder.samples
+    
+    @State
+    private var isAddReminderDialogPresented = false
+    
+    
+    @EnvironmentObject var pageModel: PageModel
+    private func presentAddReminderView() {
+        isAddReminderDialogPresented.toggle()
     }
-    .toolbar {
-      ToolbarItemGroup(placement: .bottomBar) {
-        Button(action: presentAddReminderView) {
-          HStack {
-            Image(systemName: "plus.circle.fill")
-            Text("New Reminder")
-          }
-        }
-        Spacer()
-      }
+    
+    var body: some View {
+           VStack {
+               Text("Welcome, \(pageModel.username)")
+               List($reminders) { $reminder in
+                   HStack {
+                       Image(systemName: reminder.isCompleted ? "largecircle.fill.circle" : "circle")
+                           .imageScale(.large)
+                           .foregroundColor(.accentColor)
+                           .onTapGesture {
+                               reminder.isCompleted.toggle()
+                           }
+                       Text(reminder.title)
+                   }
+               }
+               
+               Spacer() // Add Spacer to push the toolbar items to the bottom
+               
+               HStack {
+                   Spacer()
+                   Button(action: presentAddReminderView) {
+                       HStack {
+                           Image(systemName: "plus.circle.fill")
+                           Text("New Reminder")
+                       }
+                   }
+               }
+               .padding(.bottom, 8) // Adjust the padding if needed
+               .padding(.horizontal) // Add padding to the toolbar items
+               
+           }
+           .sheet(isPresented: $isAddReminderDialogPresented) {
+               AddReminderView { reminder in
+                   reminders.append(reminder)
+               }
+           }
+       }
     }
-    .sheet(isPresented: $isAddReminderDialogPresented) {
-      AddReminderView { reminder in
-        reminders.append(reminder)
-      }
-    }
-  }
-}
+
 
 struct RemindersPage_Previews: PreviewProvider {
   static var previews: some View {
